bc53fbb72037f6f601eb7e9f3169a852bc899690
kunalr22 <kunalrishi22@gmail.com>
2026-02-08 03:07:20 -0500

    textnode to htmlnode fix

--- src/textnode.py
+++ src/textnode.py
@@ -33,9 +33,9 @@ def text_node_to_html_node(text_node: TextNode):
         case TextType.CODE:
             return LeafNode("code", text_node.text)
         case TextType.LINK:
-            return LeafNode("a", text_node.text, {"href": {text_node.url}})
+            return LeafNode("a", text_node.text, {"href": text_node.url})
         case TextType.IMAGE:
-            return LeafNode("img", None, {"src": {text_node.url}, "alt": {text_node.text}})
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception("Error: invalid text type")
         
