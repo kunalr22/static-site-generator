0d7b0cac092602de70b9b0481d849553045f0863
kunalr22 <kunalrishi22@gmail.com>
2026-02-09 02:01:58 -0500

    markdown to html nodes

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from textnode import TextNode, TextType
 from markdown_parser import extract_markdown_images, extract_markdown_links, split_nodes_delimiter, split_nodes_image, split_nodes_link, text_to_textnodes
+from markdown_blocks import markdown_to_html_node
 
 
 def main():
@@ -23,10 +24,23 @@ def main():
     # print(node)
     # print(new_nodes)
 
-    text = "This is **text** with an _italic_ word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
-    nodes = text_to_textnodes(text)
+    # text = "This is **text** with an _italic_ word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+    # nodes = text_to_textnodes(text)
 
-    print(text)
-    print(nodes)
+    # print(text)
+    # print(nodes)
+
+    md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+    node = markdown_to_html_node(md)
+    html = node.to_html()
+    print("<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>")
+    print("-----------------------------------------")
+    print(html)
 
 main()
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,8 @@
 from enum import Enum
-from htmlnode import
+
+from markdown_parser import text_to_textnodes
+from textnode import text_node_to_html_node
+from htmlnode import ParentNode, LeafNode
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -42,5 +45,99 @@ def block_to_block_type(block: str):
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
+    div_children = []
     for block in blocks:
-        pass
+        block_type = block_to_block_type(block)
+        match block_type:
+            case BlockType.PARAGRAPH:
+                refined_block = block.replace("\n", " ")
+                child_nodes = markdown_to_leaf_nodes(refined_block)
+                div_children.append(ParentNode("p", child_nodes))
+            case BlockType.HEADING:
+                heading_level = heading_level_counter(block)
+                refined_block = strip_block_type_identifier(block, block_type)
+                child_nodes = markdown_to_leaf_nodes(refined_block)
+                div_children.append(ParentNode(f"h{heading_level}", child_nodes))
+            case BlockType.CODE:
+                refined_block = strip_block_type_identifier(block, block_type)
+                code_node = LeafNode("code", refined_block)
+                div_children.append(ParentNode("pre", [code_node]))
+            case BlockType.QUOTE:
+                refined_block = strip_block_type_identifier(block, block_type)
+                child_nodes = markdown_to_leaf_nodes(refined_block)
+                div_children.append(ParentNode("blockquote", child_nodes))
+            case BlockType.UNORDERED_LIST:
+                refined_block = strip_block_type_identifier(block, block_type)
+                list_children = []
+                list_items = refined_block.split("\n")
+                for item in list_items:
+                    item_children = markdown_to_leaf_nodes(item)
+                    list_children.append(ParentNode("li", item_children))
+                div_children.append(ParentNode("ul", list_children))
+            case BlockType.ORDERED_LIST:
+                refined_block = strip_block_type_identifier(block, block_type)
+                list_children = []
+                list_items = refined_block.split("\n")
+                for item in list_items:
+                    item_children = markdown_to_leaf_nodes(item)
+                    list_children.append(ParentNode("li", item_children))
+                div_children.append(ParentNode("ol", list_children))
+    return ParentNode("div", div_children)
+
+
+
+def strip_block_type_identifier(block, block_type):
+    match block_type:
+        case BlockType.HEADING:
+            space_index = block.find(" ")
+            return block[space_index + 1 :]
+        case BlockType.QUOTE:
+            lines = block.split("\n")
+            new_lines = []
+            for line in lines:
+                if line.startswith("> "):
+                    new_lines.append(line[2:])
+                else:
+                    new_lines.append(line)
+            return " ".join(new_lines)
+        case BlockType.CODE:
+            lines = block.split("\n")
+            return "\n".join(lines[1:-1]) + "\n"
+        case BlockType.UNORDERED_LIST:
+            lines = block.split("\n")
+            new_lines = []
+            for line in lines:
+                if line.startswith("- ") or line.startswith("* "):
+                    new_lines.append(line[2:])
+                else:
+                    new_lines.append(line)
+            return "\n".join(new_lines)
+        case BlockType.ORDERED_LIST:
+            lines = block.split("\n")
+            new_lines = []
+            for line in lines:
+                dot_index = line.find(". ")
+                if dot_index != -1:
+                    new_lines.append(line[dot_index + 2 :])
+                else:
+                    new_lines.append(line)
+            return "\n".join(new_lines)
+        case _:
+            return block
+
+def markdown_to_leaf_nodes(markdown):
+    text_nodes = text_to_textnodes(markdown)
+    html_nodes = []
+    for text_node in text_nodes:
+        html_nodes.append(text_node_to_html_node(text_node))
+    return html_nodes
+
+
+def heading_level_counter(text):
+    count = 0
+    for c in text:
+        if c == "#":
+            count += 1
+        else:
+            break
+    return count
